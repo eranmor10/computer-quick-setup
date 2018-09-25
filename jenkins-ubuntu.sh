17c544432d400b051b6ff4a384f3d41fccf39911
Eran <Eran.Mor@bcaa.com>
2018-09-25 11:32:45 -0700

    First edit

---
+++ jenkins-ubuntu.sh
@@ -0,0 +1,9 @@
+# This file installs Jenkins on Ubuntu
+
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+
+echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list
+
+apt-get update
+
+apt-get install jenkins
